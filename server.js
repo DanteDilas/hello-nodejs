f46dd3a6b20b938cdc54a477ee235de301af8fa2
DanteDilas <105024653+DanteDilas@users.noreply.github.com>
2022-05-23 21:01:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const http = require('http');
+const http = require("http");
+
+const host = 'localhost';
 const port = 3000;
 
-const server = http.createServer((request, response) => {
-  response.end("Hello Nodejs"
-});
+const requestListener = function (req, res) {};
 
-server.listen(port, () => {
-  console.log(`Nodejs running on PORT ${port}`)
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
 });
